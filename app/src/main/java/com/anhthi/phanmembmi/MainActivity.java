3961bdbd77ff6afa44580c90278186786f6c98cb
Vo Ngoc Anh Thi <anhthi199x@gmail.com>
2019-09-17 19:53:22 +0700

    Lam tron so thap phan

--- app/src/main/java/com/anhthi/phanmembmi/MainActivity.java
+++ app/src/main/java/com/anhthi/phanmembmi/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
     EditText edtHoTen, edtChieuCao, edtCanNang;
     Button btnTinhBMI;
@@ -26,21 +29,24 @@ public class MainActivity extends AppCompatActivity {
         btnTinhBMI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                DecimalFormat df = new DecimalFormat("#.##");
                 double H = Double.parseDouble(edtChieuCao.getText().toString());
                 double W = Double.parseDouble(edtCanNang.getText().toString());
-                final double BMI = W / Math.pow(H,2);
-                txtBMI.setText(BMI + "");
+                double BMI = W / Math.pow(H,2);
+                double kq = Double.parseDouble(df.format(BMI));
+                String ten = edtHoTen.getText().toString();
+                txtBMI.setText(kq + "");
 
                 if(BMI < 18){
-                    txtChuanDoan.setText("Người gầy");
+                    txtChuanDoan.setText(ten + " ==>" + "\n" + "Người gầy");
                 }else if(BMI >= 18 && BMI <= 24.9){
-                    txtChuanDoan.setText("Người bình thường");
+                    txtChuanDoan.setText(ten + " ==>" + "\n" + "Người bình thường");
                 }else if(BMI >= 25 && BMI <= 29.9){
-                    txtChuanDoan.setText("Người béo phì cấp độ 1");
+                    txtChuanDoan.setText(ten + " ==>" + "\n" + "Người béo phì cấp độ 1");
                 }else if(BMI >= 30 && BMI <= 34.9){
-                    txtChuanDoan.setText("Người béo phì cấp độ 2");
+                    txtChuanDoan.setText(ten + " ==>" + "\n" + "Người béo phì cấp độ 2");
                 }else {
-                    txtChuanDoan.setText("Người béo phì cấp độ 3");
+                    txtChuanDoan.setText(ten + " ==>" + "\n" + "Người béo phì cấp độ 3");
                 }
             }
         });
